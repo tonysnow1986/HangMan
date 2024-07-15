f1cac0398cb10a733c305e325f3efe1d8f08d3cc
Justin Chevalier <102847366+tonysnow1986@users.noreply.github.com>
2024-07-14 20:19:40 -0500

    hangman day 7 project

---
+++ Main.py
@@ -0,0 +1,47 @@
+from replit import clear
+import random
+from Hangman_words import word_list
+from Hangman_Art import logo, stages
+
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+# Create blanks for the game
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    clear()
+
+    # If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+        print(f"Already used that letter!!{guess}")
+    # Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    # Check if user is wrong.
+    if guess not in chosen_word:
+        # If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guess {guess}, and its wrong you lose a life!")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    # Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+    print(stages[lives])
